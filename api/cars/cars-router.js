8283c90e88297e798af11d341b7b0f6d9bbb7a1d
Rafael Deluna <delunarafael@yahoo.com>
2023-11-06 00:36:36 -0500

    Completed Sprint 14 Module 2 Guided Project

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+const Car = require('./cars-model');
+const md = require('./cars-middleware');
+
+router.get('/', async (req, res, next) => {
+    try {
+    const cars = await Car.getAll();
+    res.json(cars);
+    } catch(err) {
+        next(err);
+    }
+})
+
+router.get('/:id', md.checkCarId, async(req, res, next) => {
+    res.json(req.car);
+} )
+
+router.post('/', md.checkCarPayload, md.checkVinNumberUnique, md.checkVinNumberValid, async(req, res, next) => {
+    try {
+        const newCar = await Car.create(req.body);
+        res.status(201).json(newCar);
+    } catch(err) {
+        next(err);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
